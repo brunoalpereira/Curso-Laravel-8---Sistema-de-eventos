fae96a7825d4ed0862043b7ff1075eaadedf800e
Bruno Pereira <brunoalmeidapereira@gmail.com>
2021-12-04 12:03:19 -0300

    Feat: controller de eventos

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    
+    public function index(){
+        return view('welcome');
+    }
+
+
+    public function create(){
+        return view('events.create');
+    }
+
+}
