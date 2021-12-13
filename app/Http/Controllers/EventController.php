2383e1e381fd0c9141d6258568a67f93500ca50a
Bruno Pereira <brunoalmeidapereira@gmail.com>
2021-12-13 20:21:38 -0300

    Feat: upload de imagem criar evento

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,6 +30,19 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName(). strtotime("now")).".".$extension;
+
+            $event->image = $imageName;
+
+        }
+
         $event->save();
 
         return redirect('/')->with('msg','Evento criado com sucesso!');
