5d9240da73f271e9067f6510651cecd3740049d6
Bruno Pereira <brunoalmeidapereira@gmail.com>
2021-12-28 11:01:28 -0300

    Feat: Adicionando login com jetstream

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,12 +30,31 @@
               <li class="nav-item">
                 <a href="/events/create" class="nav-link">Criar Eventos</a>
               </li>
+              @auth
               <li class="nav-item">
-                <a href="/" class="nav-link">Entrar</a>
+                <a href="/dashboard" class="nav-link">Meus Eventos</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link">Cadastrar</a>
+                <form action="/logout" method="POST">
+                  @csrf
+                  <a href="/logout"
+                  class="nav-link"
+                  onclick="event.preventDefault();git 
+                  this.closest('form').submit();">
+                  Sair
+                  </a>
+                </form>
               </li>
+              @endauth
+              @guest
+              <li class="nav-item">
+                <a href="/login" class="nav-link">Entrar</a>
+              </li>
+           
+              <li class="nav-item">
+                <a href="/register" class="nav-link">Cadastrar</a>
+              </li>
+              @endguest
             </ul>
           </div>
         </nav>
